07dff805d0f41539050f3c89b7d7417d8a612ef7
Evan <112918649+FlinnBella@users.noreply.github.com>
2025-06-28 18:48:16 -0400

    Updated LandingPage.tsx

--- src/components/steps/LandingPage.tsx
+++ src/components/steps/LandingPage.tsx
@@ -350,10 +350,10 @@ export const LandingPage: React.FC<LandingPageProps> = ({ onGetStarted }) => {
                   <div className="ml-4 text-slate-300">bool private paid = false;</div>
                   <div className="ml-4 text-slate-300">uint256 immutable dueDate;</div>
                   <div className="ml-4 text-green-300">modifier onlyAfterDue() &#123;</div>
-                  <div className="ml-8 text-slate-300"require(block.timestamp &gt; dueDate);</div>
+                  <div className="ml-8 text-slate-300">require(block.timestamp &gt; dueDate);</div>
                   <div className="ml-8 text-slate-300">_;</div>
                   <div className="ml-4 text-green-300">&#125;</div>
-                  <div className="ml-4 text-yellow-300"event PaymentExecuted(uint256 amount);</div>
+                  <div className="ml-4 text-yellow-300">event PaymentExecuted(uint256 amount);</div>
                   <div className="text-blue-300">&#125;</div>
                 </div>
 
