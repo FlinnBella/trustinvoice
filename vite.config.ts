4d115df03067bbf4bf410fa2d3a6eea65f4feb56
Evan <112918649+FlinnBella@users.noreply.github.com>
2025-06-30 07:04:17 -0700

    Fix Vite React Fast Refresh Configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      fastRefresh: true,
+      jsxRuntime: 'automatic'
+    })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
